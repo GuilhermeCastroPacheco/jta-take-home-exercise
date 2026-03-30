0a83fd21bbf6c6dcf4d6bae25bda97be19b7ccfc
GuilhermeCastroPacheco <36938436+GuilhermeCastroPacheco@users.noreply.github.com>
2026-03-30 04:01:06 +0100

    last changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,6 +12,9 @@ import AppSidebar from './components/AppSidebar.vue'
 html, body {
   margin: 0;
   padding: 0;
+  background: var(--p-surface-50);
+  color: var(--p-text-color);
+  transition: background 0.3s, color 0.3s;
   font-family: 'Inter', sans-serif;
 }
 </style>
